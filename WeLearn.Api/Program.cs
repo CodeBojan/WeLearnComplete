d9d93b638d21f2b0b1f952447c2590d7c4f227a0
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2022-04-03 18:24:16 +0200

    [Api] Adjusted logging services.

--- WeLearn.Api/Program.cs
+++ WeLearn.Api/Program.cs
@@ -2,54 +2,65 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console());
+builder.Host.UseSerilog((ctx, lc) =>
+    lc.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(builder.Configuration));
 
 // Add services to the container.
 
-var services = builder.Services;
-
-services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
-services.AddHttpLogging(logging =>
-{
-    logging.LoggingFields =
-    Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestPath
-    | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestQuery
-    | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProtocol
-    | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestMethod;
-});
+ConfigureServices(builder);
 
 var app = builder.Build();
-var configuration = app.Configuration;
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpLogging();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-// TODO refactor to extension method
-if (configuration.GetSection("Authentication:Enabled").Get<bool>())
-{
-    // TODO
-    app.UseAuthentication();
-}
-if (configuration.GetSection("Authorization:Enabled").Get<bool>())
-{
-    // TODO
-    app.UseAuthorization();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapGet("/", () => "Hello");
+Configure(app);
 
 app.Run();
+
+static void ConfigureServices(WebApplicationBuilder builder)
+{
+    var services = builder.Services;
+
+    services.AddControllers();
+
+    // Swagger
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+    services.AddHttpLogging(logging =>
+    {
+        logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.None;
+    });
+}
+
+static void Configure(WebApplication app)
+{
+    var configuration = app.Configuration;
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        Log.Logger.Information($"Configuration:{Environment.NewLine}{(configuration as IConfigurationRoot).GetDebugView()}");
+
+        app.UseHttpLogging();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    if (configuration.GetSection("Authentication:Enabled").Get<bool>())
+    {
+        // TODO
+        app.UseAuthentication();
+    }
+    if (configuration.GetSection("Authorization:Enabled").Get<bool>())
+    {
+        // TODO
+        app.UseAuthorization();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+}
