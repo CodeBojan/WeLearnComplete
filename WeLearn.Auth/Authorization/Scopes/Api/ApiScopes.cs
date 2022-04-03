151537f067c37c38a89d77f2aeb4078733ebf805
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2022-04-03 18:24:31 +0200

    [Auth] Added some API scopes.

---
+++ WeLearn.Auth/Authorization/Scopes/Api/ApiScopes.cs
@@ -0,0 +1,24 @@
+﻿using Duende.IdentityServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace WeLearn.Auth.Authorization.Scopes.Api
+{
+    public static class ApiScopes
+    {
+        // TODO define more scopes
+        public static readonly ImmutableList<ApiScope> Scopes = ImmutableList.Create(
+            new ApiScope("year.read"),
+            new ApiScope("year.write"),
+            new ApiScope("course.read"),
+            new ApiScope("course.write"),
+            new ApiScope("content.read"),
+            new ApiScope("profile.read"),
+            new ApiScope("profile.write"),
+            
+            new ApiScope("weather.read") // TODO remove
+            );
+    }
+}
