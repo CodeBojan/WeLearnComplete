a0657bb4c23239d43c63223cb6795015c8786c2c
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2022-04-13 09:55:01 +0200

    [IS] Adjusted auth client config.

--- WeLearn.IdentityServer/Config.cs
+++ WeLearn.IdentityServer/Config.cs
@@ -31,12 +31,12 @@ public static class Config
             // interactive client using code flow + pkce
             new Client
             {
-                ClientId = "interactive",
+                ClientId = "pwa",
                 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
                 AllowedGrantTypes = GrantTypes.Code,
 
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
+                RedirectUris = { "http://localhost:3000/api/auth/callback/identityServer" },
                 FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
                 PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
 
