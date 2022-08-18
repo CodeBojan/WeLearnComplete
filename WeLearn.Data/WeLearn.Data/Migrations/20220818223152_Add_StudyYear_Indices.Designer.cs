9f2d737879226f399dab49038a2c2641929f8c59
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2022-08-19 00:33:47 +0200

    Added StudyYear indices.

--- WeLearn.Data/WeLearn.Data/Migrations/20220818223152_Add_StudyYear_Indices.Designer.cs
+++ WeLearn.Data/WeLearn.Data/Migrations/20220818223152_Add_StudyYear_Indices.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WeLearn.Data.Persistence;
@@ -11,9 +12,10 @@ using WeLearn.Data.Persistence;
 namespace WeLearn.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220818223152_Add_StudyYear_Indices")]
+    partial class Add_StudyYear_Indices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WeLearn.Data/WeLearn.Data/Migrations/20220818223152_Add_StudyYear_Indices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeLearn.Data.Migrations
+{
+    public partial class Add_StudyYear_Indices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_StudyYears_FullName",
+                table: "StudyYears",
+                column: "FullName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudyYears_ShortName",
+                table: "StudyYears",
+                column: "ShortName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_StudyYears_FullName",
+                table: "StudyYears");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudyYears_ShortName",
+                table: "StudyYears");
+        }
+    }
+}
